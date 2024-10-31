3816ebcfcf1475c6233b197e91b6951174bd8d9e
Bryan Vivas <103675860+bryansvt92@users.noreply.github.com>
2024-10-31 13:13:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 /*Este Archivo contiene el Bot de WhatsApp creado utilizando el lenguaje de cprogramación Node JS*/
+
+cont 
+
+client = new Client();
+
+client.on 
+
+
+/*esta funcion */
+client.on('authenticated', (session) => {
+//Guardamos credenciales de session para usar luego
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(), (err) => {
+      if (err) {
+          console.log(err);
+      }
+    }
+  
+}
+
+/*Condicion tenaria para verificar si la seccion esta iniciada o no*/
